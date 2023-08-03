bb48e0ecdbcc579c501254e6da5a4d9716df32ef
LuccaRig <137623677+LuccaRig@users.noreply.github.com>
2023-08-03 16:58:48 -0300

    Modules c++

---
+++ TADImplementation/Source/main.hpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "/home/luccarig/Documents/GitHub/AutobtzExercises/ChallengesC++/TADImplementation/Include/navmusic.h"
+
+
+vector<string> FilaLucca;
+NAVMUSIC Lucca = NAVMUSIC("Californication",  FilaLucca, 99);
+
+vector<string> FilaJoao;
+NAVMUSIC Joao = NAVMUSIC("Batphone", FilaJoao, 2);
+    
---
+++ TADImplementation/Source/navmusic.hpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include "/home/luccarig/Documents/GitHub/AutobtzExercises/ChallengesC++/TADImplementation/Include/navmusic.h"
+#include "main.hpp"
+
+int main(){
+    
+    Lucca.AdicionarNaFila("Pink as Floyd");
+    Lucca.AdicionarNaFila("Cant Stop");
+    Lucca.AdicionarNaFila("Under the bridge");
+    Lucca.AumentarVolume();
+    Lucca.AumentarVolume();
+    Lucca.ExibirMusicaAtual();
+    Lucca.ExibirFila();
+
+    return 0;
+}
+
+
